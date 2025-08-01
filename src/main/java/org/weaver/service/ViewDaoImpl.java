1bd0fa1977cfaabd62e4d3d66cbf8b5f18f3deb1
ryan.yeung <ryan.yeung@corpbrain.com>
2025-08-01 22:46:13 +0800

    remove import

--- src/main/java/org/weaver/service/ViewDaoImpl.java
+++ src/main/java/org/weaver/service/ViewDaoImpl.java
@@ -27,7 +27,6 @@ import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterUtils;
